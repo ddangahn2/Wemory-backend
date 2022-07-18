cc0aa1a2a4120d7826de498d92f91a7b86095df9
kdh10806 <kimdaehyun10806@gmail.com>
2022-07-19 03:43:30 +0900

    ADD : Bucket, User Model

--- wemory/settings.py
+++ wemory/settings.py
@@ -9,12 +9,13 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+from pathlib import Path
 import pymysql
 
 from my_settings import DATABASES,SECRET_KEY
-from pathlib     import Path
 
 pymysql.install_as_MySQLdb()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
